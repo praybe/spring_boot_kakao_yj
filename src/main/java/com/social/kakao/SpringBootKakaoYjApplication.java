05eb6f46c4b53e0c78ae0b8dfcc4388afdcf09a7
praybe <jungyjcool@gmail.com>
2022-03-01 02:24:04 +0900

    :chocolate_bar:

---
+++ src/main/java/com/social/kakao/SpringBootKakaoYjApplication.java
@@ -0,0 +1,13 @@
+package com.social.kakao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKakaoYjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKakaoYjApplication.class, args);
+	}
+
+}
